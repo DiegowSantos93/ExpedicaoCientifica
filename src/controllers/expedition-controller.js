e928c4fd7725f9cf66ac794da9a92a35b9db9271
DiegowSantos93 <diegowillian41@gmail.com>
2024-10-28 20:03:48 -0300

    validitation

--- src/controllers/expedition-controller.js
+++ src/controllers/expedition-controller.js
@@ -1,12 +1,27 @@
-import Expedition from "../models/expedition-model.js"
+import Expedition from "../models/expedition-model.js";
+import Explorer from "../models/explorer-model.js"; 
+import Species from "../models/species-model.js";  
 const store = async (req, res) => {
     try {
-        await Expedition.create(req.body)
-        res.json()
+        const { participants, speciesFound } = req.body; 
+        
+        const existingExplorer = await Explorer.findById(participants);
+        if (!existingExplorer) {
+            return res.status(404).json({ message: "Explorer não encontrado" });
+        }
+
+        const existingSpecies = await Species.findById(speciesFound);
+        if (!existingSpecies) {
+            return res.status(404).json({ message: "Species não encontrada" });
+        }
+
+        const expedition = await Expedition.create(req.body);
+        res.status(201).json({ message: "Expedição criada com sucesso", expedition });
+        
     } catch (error) {
-        res.status(400).json(error)
+        res.status(400).json({ message: "Erro ao criar expedição", error });
     }
-}
+};
 
 const index = async (req, res) => {
     try {
